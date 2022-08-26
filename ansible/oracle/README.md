d7c3146690f3f6a233bf13916433e7779f7cb86d
Daan de Graaf <daandegraaf9@gmail.com>
2022-08-26 11:37:05 +0200

    Deploy behind basic-auth with cast button.

--- ansible/oracle/README.md
+++ ansible/oracle/README.md
@@ -3,6 +3,8 @@ By default only SSH is enabled.
 This helps make ports 80 and 443 reachable.
 
 ## Firewall
+*Note: The Ansible playbook should take care of this.*
+
 Follow the guide 
 [here](https://blogs.oracle.com/developers/post/enabling-network-traffic-to-ubuntu-images-in-oracle-cloud-infrastructure).
 
@@ -16,5 +18,6 @@ In essence, right after the rule for SSH (look for port 22), you need to add two
 
 Then reload firewall settings with `sudo iptables-restore < /etc/iptables/rules.v4`
 
+## Security list
 You also need to enable the ports in the security list as detailed 
 [here](https://docs.oracle.com/en/learn/lab_compute_instance/index.html#connect-to-the-instance-and-install-apache-http-server).
--- src/main/java/dev/wildarch/experiments/cast/backend/Application.kt
+++ src/main/java/dev/wildarch/experiments/cast/backend/Application.kt
@@ -42,6 +42,7 @@ fun main() {
                         title {
                             +"Medialib"
                         }
+                        link(rel="stylesheet", href = "https://cdn.simplecss.org/simple.min.css")
                     }
 
                     body {
@@ -70,6 +71,7 @@ fun main() {
                         title {
                             +"Medialib - playing '$objectName'"
                         }
+                        link(rel="stylesheet", href = "https://cdn.simplecss.org/simple.min.css")
                     }
 
                     body {
@@ -78,8 +80,6 @@ fun main() {
                         }
 
                         video {
-                            width = "320"
-                            height = "240"
                             controls = true
                             autoPlay = true
 
@@ -89,6 +89,29 @@ fun main() {
                             }
 
                             +"Browser does not support the video tag"
+                        }
+
+                        button {
+                            id = "cast"
+                            +"Cast"
+                        }
+
+                        unsafe {
+                            +"""
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/castjs/5.2.0/cast.min.js"></script>
+                            <script>
+                            const cjs = new Castjs();
+
+                            // Wait for user interaction
+                            document.getElementById('cast').addEventListener('click', function() {
+                                // Check if casting is available
+                                if (cjs.available) {
+                                    // Initiate new cast session with a simple video
+                                    cjs.cast('${uri}');
+                                }
+                            });
+                            </script>
+                        """.trimIndent()
                         }
                     }
                 }
