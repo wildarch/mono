f43123c6276256a508000c2d3e19b7d272727934
Daan de Graaf <daandegraaf9@gmail.com>
2022-09-03 15:05:33 +0200

    Document uploading to object storage.

--- experiments/cast/youtubedl/README.md
+++ experiments/cast/youtubedl/README.md
@@ -3,3 +3,9 @@ To download a youtube video with subs:
 ```
 youtube-dl --write-auto-sub '<uri>'
 ```
+
+To upload a directory containing processed video files (*Also uploads files in subdirectories*):
+
+```
+oci os object bulk-upload --bucket-name <bucket name> --src-dir <directory with videos>
+```
--- experiments/cast/youtubedl/burn_subs.sh
+++ experiments/cast/youtubedl/burn_subs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Run this script within a directory of video files accompanied by subtitles (.en.vtt extension).
+# Output files are written to the parent directory.
 
 set -x
 
-for file in *.{mp4,mkv}; do
+for file in *.{mp4,mkv,webm}; do
 	output_file="../${file%.*}.mp4"
 	subs_file="${file%.*}.en.vtt"
 	if [[ -f "$output_file" ]]; then
