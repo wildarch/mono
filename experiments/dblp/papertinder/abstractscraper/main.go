6b7405e56083f292d2b9ea3a89cc4b6248443101
Daan de Graaf <daandegraaf9@gmail.com>
2022-11-14 22:10:47 +0100

    Fetch working.

--- experiments/dblp/papertinder/abstractscraper/main.go
+++ experiments/dblp/papertinder/abstractscraper/main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"time"
 
 	"github.com/andybalholm/cascadia"
 	"golang.org/x/net/html"
@@ -18,12 +19,13 @@ import (
 var dblpDbPath = flag.String("dblp-db-path", "", "path to the sqlite database file storing the dblp table")
 var reviewDbPath = flag.String("review-db-path", "", "path to the sqlite database file storing the PaperReview table")
 var testUrl = flag.String("test-url", "", "test abstract fetching on the given url")
+var prefetchLimit = flag.Int("prefetch-limit", 100, "number of abstracts to prefetch")
 
 func main() {
 	flag.Parse()
 
 	if *testUrl != "" {
-		abstract, err := fetchAbstract(*testUrl)
+		_, abstract, err := fetchAbstract(*testUrl)
 		if err != nil {
 			log.Fatalf("error fetching abstract: %s", err.Error())
 		}
@@ -31,24 +33,106 @@ func main() {
 		return
 	}
 
+	// Open review database
+	if *reviewDbPath == "" {
+		log.Fatalf("Missing required argument: --review-db-path")
+	}
+	reviewDb, err := sql.Open("sqlite", *reviewDbPath)
+	if err != nil {
+		log.Fatalf("error opening review DB at %s: %s", *reviewDbPath, err.Error())
+	}
+	defer reviewDb.Close()
+
+	// Attach the DBPL database
 	if *dblpDbPath == "" {
 		log.Fatalf("Missing required argument: --dblp-db-path")
 	}
-	dblpDb, err := sql.Open("sqlite", *dblpDbPath)
+	_, err = reviewDb.Exec("attach database ? as dblp;", *dblpDbPath)
 	if err != nil {
-		log.Fatalf("error opening dblp DB at %s: %s", *dblpDbPath, err.Error())
+		log.Fatalf("error attaching DBLP database: %s", err.Error())
 	}
-	defer dblpDb.Close()
+
+	runFetcher(reviewDb)
 }
 
-func fetchAbstract(urlS string) (string, error) {
+func runFetcher(db *sql.DB) {
+	for {
+		// Check if we have reached our limit for prefetched abstracts
+		r := db.QueryRow(`
+			SELECT COUNT(*) 
+			FROM PaperReview 
+				  -- We have attempted to fetch the abstract 
+			WHERE resolved_ee IS NOT NULL 
+				  -- But the user has not seen it yet
+			  AND interesting IS NULL`)
+		var cnt int
+		err := r.Scan(&cnt)
+		if err != nil {
+			log.Fatalf("error checking for prefetched abstracts: %s", err.Error())
+		}
+
+		if cnt >= *prefetchLimit {
+			log.Printf("We are limited to prefetching %d abstracts, but already have %d. Waiting...", *prefetchLimit, cnt)
+			time.Sleep(time.Minute)
+			continue
+		}
+
+		// Find our new candidate
+		r = db.QueryRow(`
+			SELECT key, ee
+			FROM dblp 
+			LEFT JOIN PaperReview ON (key = dblp_key) 
+				  -- We have not yet attempted to prefetch the abstract
+			WHERE resolved_ee IS NULL 
+				  -- An electronic edition is available
+			  AND ee IS NOT NULL 
+			-- Randomly take one
+			ORDER BY RANDOM()
+			LIMIT 1;
+		`)
+		var key string
+		var ee string
+		err = r.Scan(&key, &ee)
+		if err != nil {
+			log.Fatalf("error looking for a paper abstract to prefetch: %s", err.Error())
+		}
+
+		// Try to fetch the abstract
+		log.Printf("Looking for abstract for key %s, url %s", key, ee)
+		lastUrl, abstract, err := fetchAbstract(ee)
+		// Save result in the database
+		if err != nil {
+			log.Printf("Error retrieving abstract: %s", err.Error())
+
+			// Set resolved_ee but not abstract
+			_, err := db.Exec(`
+				INSERT INTO PaperReview (dblp_key, resolved_ee) VALUES (?, ?);
+			`, key, lastUrl)
+			if err != nil {
+				log.Fatalf("error inserting failed abstract fetch record: %s", err.Error())
+			}
+		} else {
+			// Set resolved_ee and abstract
+			_, err := db.Exec(`
+				INSERT INTO PaperReview (dblp_key, resolved_ee, abstract) VALUES (?, ?, ?);
+			`, key, lastUrl, abstract)
+			if err != nil {
+				log.Fatalf("error inserting abstract fetch record: %s", err.Error())
+			}
+		}
+		// sleep a bit to avoid making library servers angry
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func fetchAbstract(urlS string) (string, string, error) {
 	lastUrl, err := url.Parse(urlS)
 	if err != nil {
-		return "", fmt.Errorf("url to fetch is not valid: %w", err)
+		return lastUrl.String(), "", fmt.Errorf("url to fetch is not valid: %w", err)
 	}
 	jar, err := cookiejar.New(nil)
 	if err != nil {
-		return "", fmt.Errorf("error making cookie jar: %w", err)
+		return lastUrl.String(), "", fmt.Errorf("error making cookie jar: %w", err)
 	}
 	client := http.Client{
 		Jar: jar,
@@ -62,18 +146,20 @@ func fetchAbstract(urlS string) (string, error) {
 	}
 	resp, err := client.Get(urlS)
 	if err != nil {
-		return "", fmt.Errorf("request to %s failed: %s", urlS, err)
+		return lastUrl.String(), "", fmt.Errorf("request to %s failed: %s", urlS, err)
 	}
+	var abs string
 	switch host := lastUrl.Hostname(); host {
 	case "link.springer.com":
-		return fetchSpringerAbstract(resp)
+		abs, err = fetchSpringerAbstract(resp)
 	case "ieeexplore.ieee.org":
-		return fetchIEEEXploreAbstract(resp)
+		abs, err = fetchIEEEXploreAbstract(resp)
 	case "dl.acm.org":
-		return fetchACMAbstract(resp)
+		abs, err = fetchACMAbstract(resp)
 	default:
-		return "", fmt.Errorf("unsupported host %s", host)
+		abs, err = "", fmt.Errorf("unsupported host %s", host)
 	}
+	return lastUrl.String(), abs, err
 }
 
 func fetchSpringerAbstract(resp *http.Response) (string, error) {
