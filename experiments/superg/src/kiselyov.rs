e57ebf60dac124138b550908be16a78f10f892fe
Daan de Graaf <daandegraaf9@gmail.com>
2022-12-05 20:31:24 +0100

    Implement Fig.6 of Kiselyov.

--- experiments/superg/src/kiselyov.rs
+++ experiments/superg/src/kiselyov.rs
@@ -30,7 +30,7 @@ pub struct TypedExpr {
 }
 
 fn infer_n(e: &BExpr) -> usize {
-    match e {
+    let n = match e {
         BExpr::Var(i) => i + 1,
         BExpr::Lam(e) => {
             let n = infer_n(e);
@@ -49,13 +49,15 @@ fn infer_n(e: &BExpr) -> usize {
         BExpr::SVar(_) => 0,
         BExpr::BinOp(l, _, r) => std::cmp::max(infer_n(l), infer_n(r)),
         BExpr::Not(e) => infer_n(e),
-    }
+    };
+    println!("infer_n({:?}) = {}", e, n);
+    n
 }
 
 fn compile(e: &BExpr) -> CompiledExpr {
-    let n = infer_n(e);
-    match e {
+    let c = match e {
         BExpr::Var(i) => {
+            let n = infer_n(e);
             if n == 1 {
                 CompiledExpr::Comb(Comb::I)
             } else {
@@ -65,6 +67,7 @@ fn compile(e: &BExpr) -> CompiledExpr {
             }
         }
         BExpr::Lam(e) => {
+            let n = infer_n(e);
             if n == 0 {
                 CompiledExpr::Ap(Box::new(CompiledExpr::Comb(Comb::K)), Box::new(compile(e)))
             } else {
@@ -77,22 +80,26 @@ fn compile(e: &BExpr) -> CompiledExpr {
         BExpr::SVar(s) => CompiledExpr::Var(s.clone()),
         BExpr::BinOp(_, _, _) => todo!(),
         BExpr::Not(_) => todo!(),
-    }
+    };
+    println!("compile({:?}) = {:#?}", e, c);
+    c
 }
 
 fn semantic(n1: usize, e1: CompiledExpr, n2: usize, e2: CompiledExpr) -> CompiledExpr {
-    match (n1, n2) {
+    let dbg = format!("semantic({}, {:?}, {}, {:?}) = ", n1, e1, n2, e2);
+    let c = match (n1, n2) {
         (0, 0) => cap(e1, e2),
         (0, n2) => semantic(0, cap(Comb::B, e1), n2 - 1, e2),
         (n1, 0) => semantic(0, cap(cap(Comb::C, Comb::C), e2), n1 - 1, e1),
         (n1, n2) => semantic(
-            n1,
+            n1 - 1,
             semantic(0, CompiledExpr::Comb(Comb::S), n1 - 1, e1),
             n2 - 1,
             e2,
         ),
-        _ => todo!(),
-    }
+    };
+    println!("{}{:?}", dbg, c);
+    c
 }
 
 fn cap<F: Into<CompiledExpr>, A: Into<CompiledExpr>>(f: F, a: A) -> CompiledExpr {
@@ -160,8 +167,8 @@ mod tests {
         let parsed_expr = parse_expr(&mut tokens);
         let bexpr = to_debruijn(&parsed_expr, &mut vec![]);
         let compiled = compile(&bexpr);
-        use Comb::{B, C, I};
-        assert_eq!(compiled, cap(cap(B, cap(C, I)), I))
+        use Comb::{B, I, K, S};
+        assert_eq!(compiled, cap(cap(B, cap(S, I)), cap(cap(B, K), I)))
     }
 
     fn assert_de_bruijn_equals(expr: &str, expected_bexpr: BExpr) {
