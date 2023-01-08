1d0ab9a9191fd99d3a34ddda9183d903c364af49
Daan de Graaf <daandegraaf9@gmail.com>
2023-01-08 15:45:40 +0100

    Implement S and K.

--- experiments/superg/src/tigre/comb.rs
+++ experiments/superg/src/tigre/comb.rs
@@ -1,5 +1,8 @@
 use std::arch::global_asm;
 
+use super::{Cell, CellPtr, TigreEngine, CALL_LEN, CALL_OPCODE};
+
+// LIT combinator
 global_asm!(
     r#"
     .global comb_LIT
@@ -13,28 +16,71 @@ extern "C" {
     pub fn comb_LIT() -> usize;
 }
 
+// I combinator
 global_asm!(
     r#"
     .global comb_I
     comb_I:
-        mov rcx, [rsp]
-        add rsp, 8
-        jmp [rcx]
+        pop rax
+        jmp [rax]
 "#
 );
 extern "C" {
     pub fn comb_I() -> usize;
 }
 
+// K combinator
 global_asm!(
     r#"
-    .global comb_Abort
-    comb_Abort:
-        jmp comb_Abort
+    .global comb_K
+    comb_K:
+        mov rax, [rsp]
+        add rsp, 16
+        jmp [rax]
 "#
 );
 extern "C" {
-    pub fn comb_Abort() -> usize;
+    pub fn comb_K() -> usize;
+}
+
+// S combinator
+global_asm!(
+    r#"
+    .global comb_S
+    comb_S:
+        // Load f, g, x pointers as arguments to make_s 
+        mov rdi, [rsp]
+        mov rsi, [rsp+8]
+        mov rdx, [rsp+16]
+        call make_s
+        // Pop arguments and jump to the updated node 
+        add rsp, 24
+        jmp rax
+    "#
+);
+extern "C" {
+    pub fn comb_S() -> usize;
+}
+#[no_mangle]
+unsafe extern "C" fn make_s(f: *const CellPtr, g: *const CellPtr, x: *const CellPtr) -> CellPtr {
+    TigreEngine::with_current(|engine| {
+        // Make new cells for (f x), (g x)
+        let fx = engine.make_cell(*f, *x);
+        let gx = engine.make_cell(*g, *x);
+        // Pointers on the stack are to the right pointer of a cell, after the call instruction.
+        // To get a pointer to the full cell, we subtract the length of a call instruction.
+        let top_cell_ptr = CellPtr(x.offset(-CALL_LEN) as *mut Cell);
+        let top_cell = engine.cell_mut(top_cell_ptr);
+        top_cell.call_opcode = CALL_OPCODE;
+        top_cell.set_call_addr(fx.0 as usize);
+        top_cell.arg = gx.0 as i64;
+        top_cell_ptr
+    })
+}
+
+#[allow(non_snake_case)]
+pub extern "C" fn comb_Abort() -> usize {
+    panic!("Abort called")
 }
 
 #[cfg(test)]
--- experiments/superg/src/tigre/mod.rs
+++ experiments/superg/src/tigre/mod.rs
@@ -26,34 +26,39 @@ pub struct Cell {
     _padding: [u8; 2],
 }
 
+impl Cell {
+    pub fn set_call_addr(&mut self, target_addr: usize) {
+        let base_addr = self as *const Cell as isize + CALL_LEN;
+        let call_rel_addr = target_addr as isize - base_addr;
+        self.call_rel_addr = call_rel_addr.try_into().expect("have to jump too far");
+    }
+}
+
 const CALL_OPCODE: u8 = 0xE8;
 // OPCODE  + rel32
 const CALL_LEN: isize = 5;
 
-impl Cell {
-    fn new(lhs: i32, rhs: i32) -> Cell {
-        Cell {
-            call_opcode: CALL_OPCODE,
-            call_rel_addr: lhs,
-            arg: rhs as i64,
-            used: true,
-            _padding: [0u8; 2],
-        }
-    }
-}
-
 #[derive(Debug, Copy, Clone)]
+#[repr(transparent)]
 struct CellPtr(*mut Cell);
 
 trait IntoCellPtr {
     fn into_cell_ptr(&self, engine: &TigreEngine) -> CellPtr;
 }
 
+impl IntoCellPtr for CellPtr {
+    fn into_cell_ptr(&self, _: &TigreEngine) -> CellPtr {
+        *self
+    }
+}
+
 impl IntoCellPtr for Comb {
-    fn into_cell_ptr(&self, engine: &TigreEngine) -> CellPtr {
+    fn into_cell_ptr(&self, _: &TigreEngine) -> CellPtr {
         match self {
             Comb::I => CellPtr(comb::comb_I as *mut Cell),
             Comb::Abort => CellPtr(comb::comb_Abort as *mut Cell),
+            Comb::S => CellPtr(comb::comb_S as *mut Cell),
+            Comb::K => CellPtr(comb::comb_K as *mut Cell),
             _ => unimplemented!("into_cell_ptr {:?}", self),
         }
     }
@@ -61,13 +66,13 @@ impl IntoCellPtr for Comb {
 
 struct Lit;
 impl IntoCellPtr for Lit {
-    fn into_cell_ptr(&self, engine: &TigreEngine) -> CellPtr {
+    fn into_cell_ptr(&self, _: &TigreEngine) -> CellPtr {
         CellPtr(comb::comb_LIT as *mut Cell)
     }
 }
 
 impl IntoCellPtr for i32 {
-    fn into_cell_ptr(&self, engine: &TigreEngine) -> CellPtr {
+    fn into_cell_ptr(&self, _: &TigreEngine) -> CellPtr {
         CellPtr(*self as *mut Cell)
     }
 }
@@ -108,23 +113,34 @@ impl TigreEngine {
     }
 
     pub fn run(&mut self) -> i64 {
+        /*
+        ENGINE.with(|e| {
+            e.set(self as *mut TigreEngine);
+        });
+        */
+        unsafe { ENGINE = self as *mut TigreEngine };
         let main_ptr = self.def_lookup.get("main").expect("No main function");
         let func: fn() -> i64 = unsafe { std::mem::transmute(main_ptr.0) };
-        func()
+        let res = func();
+        /*
+        ENGINE.with(|e| {
+            e.set(std::ptr::null_mut());
+        });
+        */
+        //unsafe { ENGINE = std::ptr::null_mut() };
+        res
     }
 
     fn make_cell<CP0: IntoCellPtr, CP1: IntoCellPtr>(&mut self, lhs: CP0, rhs: CP1) -> CellPtr {
         let cell_ptr = self.alloc_cell();
 
-        let base_addr = cell_ptr.0 as isize + CALL_LEN;
-        let target_addr = lhs.into_cell_ptr(self).0 as isize;
-        let call_rel_addr = target_addr - base_addr;
+        let target_addr = lhs.into_cell_ptr(self).0 as usize;
         let arg = rhs.into_cell_ptr(self);
 
         let cell = self.cell_mut(cell_ptr);
         cell.used = true;
         cell.call_opcode = CALL_OPCODE;
-        cell.call_rel_addr = call_rel_addr.try_into().expect("Have to jump too far");
+        cell.set_call_addr(target_addr);
         cell.arg = arg.0 as i64;
         cell_ptr
     }
@@ -162,10 +178,38 @@ impl TigreEngine {
     fn alloc_expr(&mut self, expr: CompiledExpr) -> CellPtr {
         match expr {
             CompiledExpr::Int(i) => self.make_cell(Lit, i),
-            _ => unimplemented!("alloc {:?}", expr),
+            CompiledExpr::Comb(c) => c.into_cell_ptr(self),
+            CompiledExpr::Ap(l, r) => {
+                let l = self.alloc_expr(*l);
+                let r = self.alloc_expr(*r);
+                self.make_cell(l, r)
+            }
+            CompiledExpr::Var(v) => *self
+                .def_lookup
+                .get(&v)
+                .expect(&format!("unknown def '{v}'")),
         }
     }
+
+    pub fn with_current<R, F: FnOnce(&mut TigreEngine) -> R>(f: F) -> R {
+        let engine = unsafe { &mut *ENGINE };
+        f(engine)
+        /*
+        ENGINE.with(|e| {
+            let e = e.get();
+            assert!(
+                !e.is_null(),
+                "with_current must be called within a call to run"
+            );
+            let engine = unsafe { &mut *e };
+            f(engine)
+        })
+        */
+    }
 }
+
+//thread_local!(static ENGINE: std::cell::Cell<*mut TigreEngine> = std::cell::Cell::new(std::ptr::null_mut()));
+static mut ENGINE: *mut TigreEngine = std::ptr::null_mut();
 
 #[cfg(test)]
 mod tests {
@@ -180,11 +224,36 @@ mod tests {
 
     #[test]
     fn test_lit() {
-        let program = "(defun main () 42)";
+        assert_runs_to_int("(defun main () 42)", 42);
+    }
+
+    #[test]
+    fn test_id() {
+        assert_runs_to_int(
+            r#"
+(defun id (x) x)
+(defun main () (id 42))
+        "#,
+            42,
+        );
+    }
+
+    #[test]
+    fn test_k() {
+        assert_runs_to_int(
+            r#"
+(defun k (x y) x)
+(defun main () (k 42 84))
+        "#,
+            42,
+        );
+    }
+
+    fn assert_runs_to_int(program: &str, v: i64) {
         let parsed = parse(lex(program));
         let mut engine = TigreEngine::compile(&mut BracketCompiler, &parsed);
 
         let res = engine.run();
-        assert_eq!(res, 42)
+        assert_eq!(res, v);
     }
 }
