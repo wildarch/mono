f7eaa6c3e6ac0e8ac1a20e35830bb5234f8e1303
Daan de Graaf <daandegraaf9@gmail.com>
2023-01-17 15:53:39 +0100

    Benchmarking.

--- experiments/superg/benches/bench.rs
+++ experiments/superg/benches/bench.rs
@@ -1,6 +1,8 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Bencher, BenchmarkId, Criterion};
 use superg::compiled_expr::ExprCompiler;
+use superg::tigre::TigreEngine;
 
+use std::io::Write;
 use std::time::{Duration, Instant};
 use superg::bracket::BracketCompiler;
 use superg::turner::TurnerEngine;
@@ -8,42 +10,96 @@ use superg::{lex, parse, Engine};
 
 mod miranda;
 
-criterion_group!(turner, bench_fib_20);
-criterion_main!(turner, miranda::benches);
+criterion_group!(turner_vs_miranda, bench_fib_turner_vs_miranda);
+criterion_group!(turner_vs_tigre, bench_fib_turner_vs_tigre);
+criterion_main!(turner_vs_miranda, turner_vs_tigre);
 
-const FIB_20: &str = r#"
+fn fib_program(n: u16) -> String {
+    format!(
+        r#"
     (defun fib (n)
       (if (< n 2) 
           n
           (+ (fib (- n 1)) (fib (- n 2)))))
-    (defun main () (fib 20))
-"#;
-
-pub fn bench_fib_20(c: &mut Criterion) {
-    assert_runs_to_int::<_, TurnerEngine>(BracketCompiler, c, "turner_fib_20", FIB_20, 6765);
+    (defun main () (fib {n}))
+    "#
+    )
 }
 
-fn assert_runs_to_int<C: ExprCompiler, E: Engine>(
-    mut compiler: C,
-    c: &mut Criterion,
-    test_name: &str,
+fn fib(n: u16) -> i32 {
+    if n < 2 {
+        n as i32
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+}
+
+fn bench_instance<C: ExprCompiler, E: Engine>(
+    b: &mut Bencher,
     program: &str,
-    v: i32,
+    expected_res: i32,
+    mut compiler: C,
 ) {
     let parsed = parse(lex(program));
+    b.iter_custom(|iters| {
+        let mut elapsed = Duration::ZERO;
+        for _ in 0..iters {
+            let mut engine = E::compile(&mut compiler, &parsed);
+            // Measure only the time spent reducing the graph
+            let start = Instant::now();
+            let res = engine.run();
+            elapsed += start.elapsed();
+            assert_eq!(res, expected_res);
+        }
+        elapsed
+    })
+}
 
-    c.bench_function(test_name, |b| {
-        b.iter_custom(|iters| {
-            let mut elapsed = Duration::ZERO;
-            for _ in 0..iters {
-                let mut engine = E::compile(&mut compiler, &parsed);
-                // Measure only the time spent reducing the graph
-                let start = Instant::now();
-                let res = engine.run();
-                elapsed += start.elapsed();
-                assert_eq!(res, v);
-            }
-            elapsed
-        })
-    });
+fn bench_fib_turner_vs_miranda(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Fibonacci (Turner vs. Miranda)");
+    for n in (10..=20).step_by(2) {
+        let expected_res = fib(n);
+        // Turner
+        group.bench_with_input(BenchmarkId::new("TurnerEngine (Bracket)", n), &n, |b, n| {
+            bench_instance::<_, TurnerEngine>(b, &fib_program(*n), expected_res, BracketCompiler);
+        });
+
+        // Miranda
+        group.bench_with_input(BenchmarkId::new("Miranda", n), &n, |b, n| {
+            // Create the temp file with the module contents
+            let mut temp_file = tempfile::Builder::new().suffix(".m").tempfile().unwrap();
+            write!(
+                temp_file,
+                r#"
+fib n = n,                              if n < 2
+      = fib(n - 1) + fib(n - 2),        otherwise
+    "#
+            )
+            .unwrap();
+            temp_file.flush().unwrap();
+            b.iter_custom(|iters| {
+                let mut elapsed = Duration::ZERO;
+                for _ in 0..iters {
+                    let stats = miranda::run_miranda(temp_file.path(), &format!("fib {n}"));
+                    elapsed += stats.runtime;
+                    assert_eq!(stats.result, expected_res);
+                }
+                elapsed
+            });
+        });
+    }
+}
+
+fn bench_fib_turner_vs_tigre(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Fibonacci (Turner vs. TIGRE)");
+    for n in (10..=20).step_by(2) {
+        let expected_res = fib(n);
+        // Turner
+        group.bench_with_input(BenchmarkId::new("TurnerEngine (Bracket)", n), &n, |b, n| {
+            bench_instance::<_, TurnerEngine>(b, &fib_program(*n), expected_res, BracketCompiler);
+        });
+        group.bench_with_input(BenchmarkId::new("TigreEngine (Bracket)", n), &n, |b, n| {
+            bench_instance::<_, TigreEngine>(b, &fib_program(*n), expected_res, BracketCompiler);
+        });
+    }
 }
--- experiments/superg/benches/miranda.rs
+++ experiments/superg/benches/miranda.rs
@@ -1,59 +1,24 @@
-use criterion::{criterion_group, Criterion};
 use regex::Regex;
 use std::io::Write;
 use std::path::Path;
 use std::process::{Command, Stdio};
 use std::time::Duration;
 
-criterion_group!(benches, bench_fib_20);
-
-pub fn bench_fib_20(c: &mut Criterion) {
-    bench_fib(20, c)
-}
-
-fn bench_fib(n: u16, c: &mut Criterion) {
-    bench_miranda(
-        c,
-        &format!("miranda_fib_{n}"),
-        r#"
-fib n = n,                              if n <= 2
-      = fib(n - 1) + fib(n - 2),        otherwise
-        "#,
-        &format!("fib {n}"),
-    );
-}
-
-fn bench_miranda(c: &mut Criterion, test_name: &str, module_file_content: &str, command: &str) {
-    // Create the temp file with the module contents
-    let mut temp_file = tempfile::Builder::new().suffix(".m").tempfile().unwrap();
-    write!(temp_file, "{}", module_file_content).unwrap();
-    temp_file.flush().unwrap();
-    c.bench_function(test_name, |b| {
-        b.iter_custom(|iters| {
-            let mut elapsed = Duration::ZERO;
-
-            for _ in 0..iters {
-                elapsed += run_miranda(temp_file.path(), command).runtime;
-            }
-            elapsed
-        })
-    });
-}
-
 #[derive(Debug, Clone, Copy, PartialEq)]
-struct Stats {
-    runtime: Duration,
-    cycles: usize,
+pub struct Stats {
+    pub runtime: Duration,
+    pub cycles: usize,
+    pub result: i32,
 }
 
-fn run_miranda(file_path: &Path, command: &str) -> Stats {
+pub fn run_miranda(file_path: &Path, command: &str) -> Stats {
     let miranda_path = std::env::var("MIRANDA_PATH").expect("'MIRANDA_PATH' must be set");
     let mut mira = Command::new("./mira")
         .arg(file_path)
         .current_dir(miranda_path)
         .stdin(Stdio::piped())
-        .stdout(Stdio::null())
-        .stderr(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit())
         .spawn()
         .expect("error starting miranda");
     {
@@ -64,17 +29,19 @@ fn run_miranda(file_path: &Path, command: &str) -> Stats {
 
     let output = mira.wait_with_output().expect("error running miranda");
 
-    let stderr = String::from_utf8(output.stderr).unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
 
-    let stats_regex = Regex::new("Nanos: (\\d+)\nCycles: (\\d+)\n").unwrap();
-    let caps = match stats_regex.captures(&stderr) {
+    let stats_regex = Regex::new("Nanos: (\\d+)\nCycles: (\\d+)\n(\\d+)").unwrap();
+    let caps = match stats_regex.captures(&stdout) {
         Some(caps) => caps,
-        None => panic!("stats not found, stderr: {}", stderr),
+        None => panic!("stats not found, stdout: {}", stdout),
     };
     let nanos: u64 = caps.get(1).unwrap().as_str().parse().unwrap();
     let cycles = caps.get(2).unwrap().as_str().parse().unwrap();
+    let result = caps.get(3).unwrap().as_str().parse().unwrap();
     Stats {
         runtime: Duration::from_nanos(nanos),
         cycles,
+        result,
     }
 }
