a6f643a8270113f7861d62f9ed876e7d0eeefb4b
Daan de Graaf <daandegraaf9@gmail.com>
2023-01-24 12:53:20 +0100

    Make list of papers.

---
+++ experiments/phd/README.md
@@ -0,0 +1,7 @@
+# Potential PHD supervisors in NL 
+
+To download XMLs for all DBLP person records:
+
+```shell
+xargs -n 1 curl -O < ../urls.txt
+```
---
+++ experiments/phd/papers.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import os
+from lxml import etree
+from dataclasses import dataclass
+
+PEOPLE_DIR='people/'
+
+@dataclass
+class Paper:
+    key: str
+    title: str
+    authors: list[str]
+    year: int
+    ee: str
+
+papers = {}
+
+for xml_filename in os.listdir(PEOPLE_DIR):
+    xml_path = PEOPLE_DIR + xml_filename
+    tree = etree.parse(xml_path)
+
+    for r in tree.iterfind('//r/*'):
+        title = etree.tostring(r.find('.//title')).decode('utf-8')
+        title = title.strip().replace('<title>', '').replace('</title>', '')
+        paper = Paper(
+            key = r.get('key'),
+            title = title,
+            authors = [a.text for a in r.iterfind('.//author')],
+            year = int(r.findtext('.//year')),
+            ee = r.findtext('.//ee'),
+        )
+
+        papers[paper.key] = paper
+
+print(len(papers))
+
+papers = list(papers.values())
+papers.sort(key=lambda p: p.year, reverse=True)
+
+with open('papers.html', 'w') as f:
+    f.write('''
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Papers</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<body>
+<h1>Papers</h1>
+
+    ''')
+
+    year = papers[0].year
+    f.write(f'<h2>{year}</h2>')
+    for paper in papers:
+        if paper.year != year:
+            year = paper.year
+            f.write(f'<h2>{year}</h2>')
+        
+        f.write('<div>')
+        f.write(f'<a href="{paper.ee}">{paper.title}</a> {", ".join(paper.authors)}')
+        f.write('<br />')
+        f.write('<br />')
+        f.write('</div>')
+
+    f.write('''
+</body>
+</html>
+    ''')
