a8a4a501348501939fa16d77b2480f55560104cb
Daan de Graaf <daandegraaf9@gmail.com>
2023-01-29 00:22:04 +0100

    Better plots.

--- experiments/superg/doc/paper/benches/typeset.py
+++ experiments/superg/doc/paper/benches/typeset.py
@@ -6,29 +6,21 @@ Parses Criterion benchmark results for superg, and outputs a Latex/pgfplots-frie
 import sys
 import json
 
-ENGINES = {
-    "Miranda": "Miranda (original)",
-    "TurnerEngine": "Miranda-style Engine",
-    "TigreEngine": "TIGRE-style Engine",
-}
-
-BENCHMARKS = {
-    "Fibonacci (Turner vs. Miranda)",
-    "Fibonacci (Turner vs. TIGRE)",
-    "Fibonacci n (All compilers)",
-    "Ackermann 3 n (All compilers)",
-}
+BASE_DIR="experiments/superg/doc/paper/benches"
 
 COMPILERS = {
-    "Bracket",
-    "Kiselyov - Strict",
-    "Kiselyov - Lazy",
-    "Kiselyov - LazyOpt",
-    "Kiselyov - Linear",
+    "Bracket": "Bracket Abstraction",
+    "Kiselyov - Strict": "Kiselyov - $\\strict$",
+    "Kiselyov - Lazy": "Kiselyov - $\\lazy$",
+    "Kiselyov - LazyOpt": "Kiselyov - $\\lazyeta$",
+    "Kiselyov - Linear": "Kiselyov - $\\linear$",
 }
 
 miranda_coords = []
 turner_coords = []
+tigre_coords = []
+
+compiler_coords = {c: [] for c in COMPILERS.values()}
 
 def handle_message(msg):
     if msg["reason"] != "benchmark-complete":
@@ -38,42 +30,89 @@ def handle_message(msg):
     score = msg["typical"]["estimate"]
 
     # Turner v. Miranda
-    if benchmark != "Fibonacci (Turner vs. Miranda)":
-        return
-    if config == "Miranda":
-        miranda_coords.append((val, score))
-    elif config == "TurnerEngine (Bracket)":
-        turner_coords.append((val, score))
+    if benchmark == "Fibonacci (Turner vs. Miranda)":
+        if config == "Miranda":
+            miranda_coords.append((val, score))
+        elif config == "TurnerEngine (Bracket)":
+            turner_coords.append((val, score))
+    # Turner v. TIGRE
+    if benchmark == "Fibonacci (Turner vs. TIGRE)":
+        if config == "TigreEngine (Bracket)":
+            tigre_coords.append((val, score))
+        # Turner results are the same as Turner v. Miranda
+    
+    if benchmark == "Fibonacci n (All compilers)":
+        compiler = COMPILERS[config]
+        assert(compiler is not None)
+
+        compiler_coords[compiler].append((val, score))
     
 
-with open(sys.argv[1]) as f:
+with open(BASE_DIR + "/output.json") as f:
     for line in f:
         msg = json.loads(line)
         handle_message(msg)
 
-print("=== Original Miranda vs. Turner ===")
-print("""
-\\addplot[
-    color = blue,
-    mark=square,
+with open(BASE_DIR + "/miranda_vs_turner.tex", "w") as f:
+    f.write("""
+    \\addplot[
+        color=blue,
+        mark=*,
     ]
-    coordinates {""")
-for (val, ns) in miranda_coords:
-    ms = ns / 1000.0 / 1000.0
-    print(f"        ({val}, {ms})")
-print("""    };
-""")
+        coordinates {""")
+    for (val, ns) in turner_coords:
+        ms = ns / 1000.0 / 1000.0
+        f.write(f"        ({val}, {ms})")
+    f.write("""    };
+    """)
 
-print("""
-\\addplot[
-    color = red,
-    mark=o,
+    f.write("""
+    \\addplot
+        coordinates {""")
+    for (val, ns) in miranda_coords:
+        ms = ns / 1000.0 / 1000.0
+        f.write(f"        ({val}, {ms})")
+    f.write("""    };
+    """)
+
+
+    f.write("\\legend{Miranda(original), Miranda-style Engine}")
+
+with open(BASE_DIR + "/turner_vs_tigre.tex", "w") as f:
+    f.write("""
+    \\addplot[
+        color=blue,
+        mark=*,
     ]
-    coordinates {""")
-for (val, ns) in turner_coords:
-    ms = ns / 1000.0 / 1000.0
-    print(f"        ({val}, {ms})")
-print("""    };
-""")
+        coordinates {""")
+    for (val, ns) in turner_coords:
+        ms = ns / 1000.0 / 1000.0
+        f.write(f"        ({val}, {ms})")
+    f.write("""    };
+    """)
 
-print("\\legend{Miranda(original), Miranda-style Engine}")
+    f.write("""
+    \\addplot
+        coordinates {""")
+    for (val, ns) in tigre_coords:
+        ms = ns / 1000.0 / 1000.0
+        f.write(f"        ({val}, {ms})")
+    f.write("""    };
+    """)
+
+    f.write("\\legend{Miranda-style Engine, TIGRE-style Engine}")
+
+with open(BASE_DIR + "/bracket_vs_kiselyov.tex", "w") as f:
+    for c in COMPILERS.values():
+        f.write("""
+        \\addplot
+            coordinates {""")
+        for (val, ns) in compiler_coords[c]:
+            ms = ns / 1000.0 / 1000.0
+            f.write(f"        ({val}, {ms})")
+        f.write("""    };
+        """)
+
+
+    legend = ",".join(COMPILERS.values())
+    f.write(f"\\legend{{{legend}}}")
