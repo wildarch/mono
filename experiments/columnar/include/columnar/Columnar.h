f8c8c03e72e3aba48505ce911ebb70bbd93863a5
Daan de Graaf <daandegraaf9@gmail.com>
2024-11-09 20:02:33 +0100

    AggregateOp.

--- experiments/columnar/include/columnar/Columnar.h
+++ experiments/columnar/include/columnar/Columnar.h
@@ -4,8 +4,15 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/Region.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
 
 #include "columnar/Dialect.h.inc"
+
+#include "columnar/Enums.h.inc"
+
+#define GET_ATTRDEF_CLASSES
+#include "columnar/Attrs.h.inc"
 
 #define GET_TYPEDEF_CLASSES
 #include "columnar/Types.h.inc"
@@ -13,4 +20,9 @@
 #define GET_OP_CLASSES
 #include "columnar/Ops.h.inc"
 
-namespace columnar {} // namespace columnar
+namespace columnar {
+
+/** Element type for columns produced by a COUNT aggregator. */
+mlir::Type getCountElementType(mlir::MLIRContext *ctx);
+
+} // namespace columnar
--- experiments/columnar/lib/Columnar.cpp
+++ experiments/columnar/lib/Columnar.cpp
@@ -8,6 +8,11 @@
 
 #include "columnar/Dialect.cpp.inc"
 
+#include "columnar/Enums.cpp.inc"
+
+#define GET_ATTRDEF_CLASSES
+#include "columnar/Attrs.cpp.inc"
+
 #define GET_TYPEDEF_CLASSES
 #include "columnar/Types.cpp.inc"
 
@@ -15,6 +20,10 @@
 #include "columnar/Ops.cpp.inc"
 
 namespace columnar {
+
+mlir::Type getCountElementType(mlir::MLIRContext *ctx) {
+  return mlir::IntegerType::get(ctx, /*width=*/64, mlir::IntegerType::Signed);
+}
 
 void ColumnarDialect::initialize() {
   addOperations<
@@ -26,6 +35,66 @@ void ColumnarDialect::initialize() {
 #define GET_TYPEDEF_LIST
 #include "columnar/Types.cpp.inc"
       >();
+
+  addAttributes<
+#define GET_ATTRDEF_LIST
+#include "columnar/Attrs.cpp.inc"
+      >();
+}
+
+mlir::LogicalResult QueryOutputOp::verify() {
+  if (getColumns().size() != getNames().size()) {
+    return emitOpError("Outputs ")
+           << getColumns().size() << " columns, but got " << getNames().size()
+           << " column names";
+  }
+
+  return mlir::success();
+}
+
+static ColumnType aggregatorType(ColumnType input, Aggregator agg) {
+  switch (agg) {
+  // Same return type regardless of input.
+  case Aggregator::COUNT:
+  case Aggregator::COUNT_DISTINCT: {
+    auto *ctx = input.getContext();
+    return ColumnType::get(ctx, getCountElementType(ctx));
+  }
+  // Monoids, same output type as input
+  case Aggregator::SUM:
+  case Aggregator::AVG:
+  case Aggregator::MIN:
+    return input;
+  }
+
+  llvm_unreachable("invalid enum");
+}
+
+mlir::LogicalResult AggregateOp::inferReturnTypes(
+    mlir::MLIRContext *ctx, std::optional<mlir::Location> location,
+    Adaptor adaptor, llvm::SmallVectorImpl<mlir::Type> &inferredReturnTypes) {
+  // Keep types from the group-by
+  for (auto groupBy : adaptor.getGroupBy()) {
+    inferredReturnTypes.push_back(groupBy.getType());
+  }
+
+  for (auto [col, agg] :
+       llvm::zip_equal(adaptor.getAggregate(), adaptor.getAggregators())) {
+    inferredReturnTypes.push_back(
+        aggregatorType(llvm::cast<ColumnType>(col.getType()), agg));
+  }
+
+  return mlir::success();
+}
+
+mlir::LogicalResult AggregateOp::verify() {
+  if (getAggregate().size() != getAggregators().size()) {
+    return emitOpError("has ")
+           << getAggregate().size() << " columns to aggregate, but "
+           << getAggregators().size() << " aggregators";
+  }
+
+  return mlir::success();
 }
 
 } // namespace columnar
