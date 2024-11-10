676fe5edc61e6d64dd24f0986d88426bb682f6e5
Daan de Graaf <daandegraaf9@gmail.com>
2024-11-10 11:17:02 +0100

    Lots of small things.

--- experiments/columnar/mlir-opt.cpp
+++ experiments/columnar/mlir-opt.cpp
@@ -1,9 +1,12 @@
 #include <columnar/Columnar.h>
 #include <mlir/Tools/mlir-opt/MlirOptMain.h>
+#include <mlir/Transforms/Passes.h>
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   registry.insert<columnar::ColumnarDialect>();
+
+  mlir::registerCanonicalizer();
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "Columnar MLIR opt\n", registry));
