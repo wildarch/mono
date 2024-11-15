a353f40d1bf48909758e1a225ee3c6a53568af0d
Daan de Graaf <daandegraaf9@gmail.com>
2024-11-15 22:05:49 +0100

    setup lit tests.

---
+++ experiments/columnar/test/lit.cfg.py
@@ -0,0 +1,27 @@
+from lit.llvm import llvm_config
+import lit.formats
+
+config.name = "Columnar"
+
+config.test_format = lit.formats.ShTest(execute_external=False)
+
+config.suffixes = ['.mlir']
+
+# Find common tools such as FileCheck
+llvm_config.use_default_substitutions()
+
+# The tools we want to use in lit test (inside RUN)
+tools = [
+    "mlir-opt",
+]
+
+# Where we look for the tools
+tool_dirs = [
+    config.llvm_tools_dir,
+    config.mlir_tools_dir,
+    config.columnar_tools_dir,
+]
+
+llvm_config.add_tool_substitutions(tools, tool_dirs)
+
+config.environment["FILECHECK_OPTS"] = "--enable-var-scope --allow-unused-prefixes=false"
