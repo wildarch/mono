073a3d39cebb81025c88c3d444eaf916eb6f6350
Daan de Graaf <daandegraaf9@gmail.com>
2024-11-24 22:30:53 +0100

    Basic SQL conversion test.

--- experiments/columnar/test/sql/scan.sql
+++ experiments/columnar/test/sql/scan.sql
@@ -1,3 +1,6 @@
-SELECT name
-FROM City
-WHERE id = 42;
+-- RUN: translate --import-sql %s | FileCheck %s
+
+-- CHECK: %[[#PARTKEY:]] = columnar.read_table "part" "p_partkey" : <i64>
+-- CHECK: columnar.query.output %[[#PARTKEY]] {{.*}} ["partkey"]
+SELECT p_partkey AS partkey
+FROM Part;
