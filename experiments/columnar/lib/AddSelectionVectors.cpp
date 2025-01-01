4af4aac04ea2fbd27caaf2ae4d223f23cc558efa
Daan de Graaf <daandegraaf9@gmail.com>
2025-01-01 17:43:47 +0100

    Nice progress.

--- experiments/columnar/lib/AddSelectionVectors.cpp
+++ experiments/columnar/lib/AddSelectionVectors.cpp
@@ -21,89 +21,39 @@ public:
   void runOnOperation() final;
 };
 
-struct WithSel {
-  mlir::Value sel;
-  llvm::SmallVector<mlir::Value> columns;
-};
-
-class SelectionVectorTracker {
+class ExtractCommonSel {
 private:
-  llvm::DenseMap<mlir::Value, mlir::Value> _resolved;
+  mlir::Value sel;
 
 public:
-  void createTableReadSelectionVectors(QueryOp query,
-                                       mlir::RewriterBase &rewriter);
-
-  bool allInputsResolvedOrConstant(mlir::ValueRange inputs);
-  mlir::FailureOr<WithSel> getOrCreateInputs(mlir::ValueRange inputs,
-                                             mlir::RewriterBase &rewriter);
-
-  bool isResolved(mlir::Value v);
+  mlir::Value input(mlir::Value v);
+  mlir::Value getCommonSelectionVector();
 };
 
 } // namespace
 
-void SelectionVectorTracker::createTableReadSelectionVectors(
-    QueryOp query, mlir::RewriterBase &rewriter) {
-
-  // All tables referenced in the query.
-  llvm::SmallDenseSet<mlir::StringAttr> tables;
-  query->walk([&](ReadTableOp op) { tables.insert(op.getTableAttr()); });
-
-  // Create selection vectors
-  llvm::SmallDenseMap<mlir::StringAttr, mlir::Value> selPerTable;
-  mlir::OpBuilder::InsertionGuard guard(rewriter);
-  rewriter.setInsertionPointToStart(&query.getBody().front());
-  for (auto table : tables) {
-    selPerTable[table] =
-        rewriter.create<SelTableOp>(rewriter.getUnknownLoc(), table);
-  }
-
-  // Mark as resolved
-  query->walk([&](ReadTableOp op) {
-    _resolved[op] = selPerTable.at(op.getTableAttr());
-  });
-}
-
-bool SelectionVectorTracker::allInputsResolvedOrConstant(
-    mlir::ValueRange inputs) {
-  return llvm::all_of(inputs, [this](mlir::Value v) {
-    return _resolved.contains(v) || v.getDefiningOp<ConstantOp>();
-  });
-}
-
-mlir::FailureOr<WithSel>
-SelectionVectorTracker::getOrCreateInputs(mlir::ValueRange inputs,
-                                          mlir::RewriterBase &rewriter) {
-  WithSel result{
-      .columns = inputs,
-  };
-  for (auto input : inputs) {
-    if (auto inSel = _resolved.lookup(input)) {
-      if (!result.sel) {
-        result.sel = inSel;
-      } else {
-        return mlir::emitError(input.getLoc())
-               << "input " << input << " has resolved selection vector "
-               << inSel
-               << ", but the selection vector was previously resolved as "
-               << result.sel;
-      }
+mlir::Value ExtractCommonSel::input(mlir::Value v) {
+  if (auto applyOp = v.getDefiningOp<SelApplyOp>()) {
+    if (!sel || sel == applyOp.getSel()) {
+      // Compatible selection vectors.
+      sel = applyOp.getSel();
+      return applyOp.getInput();
     } else {
-      assert(input.getDefiningOp<ConstantOp>());
+      // Incompatible
+      sel = nullptr;
+      return v;
     }
+  } else if (v.getDefiningOp<ConstantOp>()) {
+    // Any selection vector is OK.
+    return v;
+  } else {
+    // No selection vector found.
+    sel = nullptr;
+    return v;
   }
-
-  if (!result.sel) {
-    result.sel = rewriter.create<SelConstOp>(rewriter.getUnknownLoc());
-  }
-
-  return result;
 }
 
-bool SelectionVectorTracker::isResolved(mlir::Value v) {
-  return _resolved.contains(v);
-}
+mlir::Value ExtractCommonSel::getCommonSelectionVector() { return sel; }
 
 /*
 Base reads:
@@ -127,62 +77,175 @@ vectors on input sides.
 - LimitOp: Caps only the selection vector.
 */
 
-static mlir::LogicalResult resolve(SelectionVectorTracker &tracker,
-                                   mlir::Operation *op,
-                                   mlir::RewriterBase &rewriter) {
-  if (llvm::all_of(op->getResults(),
-                   [&](mlir::Value v) { return tracker.isResolved(v); })) {
-    // Already resolved
-    return mlir::success();
+static mlir::Value clonePredicate(PredicateOp op, mlir::ValueRange inputs,
+                                  mlir::PatternRewriter &rewriter) {
+  llvm::SmallVector<mlir::Value> predInputs;
+  for (auto in : op.getInputs()) {
+    auto arg = llvm::cast<mlir::BlockArgument>(in);
+    predInputs.push_back(inputs[arg.getArgNumber()]);
   }
 
-  return op->emitOpError("not implemented");
+  mlir::IRMapping mapping;
+  mapping.map(op.getBody().getArguments(), predInputs);
+
+  for (auto &op : op.getBody().front()) {
+    if (auto evalOp = llvm::dyn_cast<PredicateEvalOp>(op)) {
+      return mapping.lookup(evalOp.getCond());
+    }
+
+    auto *newOp = rewriter.clone(op, mapping);
+    mapping.map(&op, newOp);
+  }
+
+  llvm_unreachable("Missing eval op");
+}
+
+static llvm::SmallVector<mlir::Value>
+applySel(mlir::ValueRange inputs, mlir::Value sel,
+         mlir::PatternRewriter &rewriter) {
+  llvm::SmallVector<mlir::Value> newInputs;
+  for (auto input : inputs) {
+    newInputs.push_back(
+        rewriter.create<SelApplyOp>(input.getLoc(), input, sel));
+  }
+
+  return newInputs;
+}
+
+static mlir::LogicalResult lowerSelect(SelectOp op,
+                                       mlir::PatternRewriter &rewriter) {
+  auto inputOp = rewriter.create<SelAddOp>(op.getLoc(), op.getInputs());
+  auto sel = inputOp.getSel();
+
+  auto inputs = applySel(op.getInputs(), sel, rewriter);
+
+  for (auto pred : op.getPredicates().getOps<PredicateOp>()) {
+    llvm::SmallVector<mlir::Value> predInputs;
+    for (auto in : pred.getInputs()) {
+      auto arg = llvm::cast<mlir::BlockArgument>(in);
+      assert(arg.getOwner() == &op.getPredicates().front());
+      predInputs.push_back(inputs[arg.getArgNumber()]);
+    }
+
+    auto filter = clonePredicate(pred, inputs, rewriter);
+    auto filterSel =
+        rewriter.create<ConstantOp>(op.getLoc(), rewriter.getAttr<SelIdAttr>());
+    auto filterOp =
+        rewriter.create<SelFilterOp>(op.getLoc(), sel, filter, filterSel);
+    sel = filterOp.getOutSel();
+
+    inputs = applySel(op.getInputs(), sel, rewriter);
+  }
+
+  rewriter.replaceOp(op, inputs);
+  return mlir::success();
+}
+
+static bool readWithIdentitySel(mlir::Value v) {
+  auto *op = v.getDefiningOp();
+  return op && llvm::isa<ConstantOp, ReadTableOp>(op);
+}
+
+// If all inputs can be read using an identity selection vector, use that.
+static mlir::LogicalResult selAddIdent(SelAddOp op,
+                                       mlir::PatternRewriter &rewriter) {
+  if (!llvm::all_of(op.getInputs(), readWithIdentitySel)) {
+    return mlir::failure();
+  }
+
+  auto idSel =
+      rewriter.create<ConstantOp>(op.getLoc(), rewriter.getAttr<SelIdAttr>());
+  rewriter.replaceAllUsesWith(op.getSel(), idSel);
+  rewriter.replaceAllUsesWith(op.getResults(), op.getInputs());
+  return mlir::success();
+}
+
+static mlir::LogicalResult applyCmp(CmpOp op, mlir::PatternRewriter &rewriter) {
+  if (op.getSel()) {
+    return mlir::failure();
+  }
+
+  ExtractCommonSel extract;
+  auto lhs = extract.input(op.getLhs());
+  auto rhs = extract.input(op.getRhs());
+  auto sel = extract.getCommonSelectionVector();
+  if (!sel) {
+    return mlir::failure();
+  }
+
+  auto newOp = rewriter.create<CmpOp>(op.getLoc(), op.getPred(), lhs, rhs, sel);
+  rewriter.replaceOpWithNewOp<SelApplyOp>(op, newOp, sel);
+  return mlir::success();
+}
+
+template <typename T>
+static mlir::LogicalResult
+applyNumericalBinOp(T op, mlir::PatternRewriter &rewriter) {
+  if (op.getSel()) {
+    return mlir::failure();
+  }
+
+  ExtractCommonSel extract;
+  auto lhs = extract.input(op.getLhs());
+  auto rhs = extract.input(op.getRhs());
+  auto sel = extract.getCommonSelectionVector();
+  if (!sel) {
+    return mlir::failure();
+  }
+
+  auto newOp = rewriter.create<T>(op.getLoc(), lhs, rhs, sel);
+  rewriter.replaceOpWithNewOp<SelApplyOp>(op, newOp, sel);
+  return mlir::success();
+}
+
+// Whether the value represents the identity selection vector.
+static bool isIdentitySel(mlir::Value v) {
+  return mlir::matchPattern(v, mlir::m_Constant<SelIdAttr>(nullptr));
+}
+
+static mlir::LogicalResult applyFilter(SelFilterOp op,
+                                       mlir::PatternRewriter &rewriter) {
+  auto applyOp = op.getFilter().getDefiningOp<SelApplyOp>();
+  if (!applyOp || !isIdentitySel(op.getFilterSel())) {
+    return mlir::failure();
+  }
+
+  rewriter.modifyOpInPlace(op, [&]() {
+    op.getFilterMutable().assign(applyOp.getInput());
+    op.getFilterSelMutable().assign(applyOp.getSel());
+  });
+  return mlir::success();
 }
 
 void AddSelectionVectors::runOnOperation() {
-  // 1. Add all ops to a worklist.
-  llvm::SmallPtrSet<mlir::Operation *, 16> worklist;
-  for (auto &op : getOperation().getBody().front()) {
-    worklist.insert(&op);
-  }
+  mlir::RewritePatternSet patterns(&getContext());
+  patterns.add(lowerSelect);
+  patterns.add(selAddIdent);
+  patterns.add(applyCmp);
+  patterns.add(applyFilter);
+  patterns.add(applyNumericalBinOp<AddOp>);
+  patterns.add(applyNumericalBinOp<SubOp>);
+  patterns.add(applyNumericalBinOp<MulOp>);
+  patterns.add(applyNumericalBinOp<DivOp>);
 
-  SelectionVectorTracker tracker;
-  mlir::IRRewriter rewriter(getOperation());
-  tracker.createTableReadSelectionVectors(getOperation(), rewriter);
-
-  // 2. Iterate on the worklist:
-  //   a. find an operation to process: All inputs have to have selection
-  //   vectors available.
-  //   b. if no operation is found, break out of the loop
-  //   c. remove the operation from the worklist
-  //   d. rewrite the ops to use selection vectors
-  while (!worklist.empty()) {
-    auto nextOp = llvm::find_if(worklist, [&](mlir::Operation *op) {
-      return tracker.allInputsResolvedOrConstant(op->getOperands());
-    });
-
-    if (nextOp == worklist.end()) {
-      break;
-    }
-
-    auto *op = *nextOp;
-    worklist.erase(op);
-
-    // TODO
-    if (mlir::failed(resolve(tracker, op, rewriter))) {
-      op->emitOpError("failed to resolve selection vectors");
-      return signalPassFailure();
-    }
-  }
-
-  // 3. If the worklist is empty, the algorithm has completed.
-  if (!worklist.empty()) {
-    for (auto *op : worklist) {
-      op->emitOpError("selection vectors of inputs remained unresolved");
-    }
-
+  if (mlir ::failed(mlir::applyPatternsAndFoldGreedily(getOperation(),
+                                                       std::move(patterns)))) {
     return signalPassFailure();
   }
+
+  getOperation()->walk([](SelAddOp op) {
+    auto diag = op->emitOpError("remains after rewrites");
+    for (auto *user : op->getUsers()) {
+      diag.attachNote(user->getLoc()) << "used by " << user;
+    }
+  });
+
+  getOperation()->walk([](SelApplyOp op) {
+    auto diag = op->emitWarning("op 'sel.apply' remains after rewrites");
+    for (auto *user : op->getUsers()) {
+      diag.attachNote(user->getLoc()) << "used by " << user;
+    }
+  });
 }
 
 } // namespace columnar
