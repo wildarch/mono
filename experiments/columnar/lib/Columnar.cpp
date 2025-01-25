b1bc290c303f4b8b7f896c7360daac033218f19f
Daan de Graaf <daandegraaf9@gmail.com>
2025-01-25 21:27:36 +0100

    Track termination.

--- experiments/columnar/lib/Columnar.cpp
+++ experiments/columnar/lib/Columnar.cpp
@@ -361,16 +361,10 @@ mlir::LogicalResult SelAddOp::inferReturnTypes(
 
 mlir::OpFoldResult SelTableOp::fold(FoldAdaptor adaptor) { return getTable(); }
 
-void ChunkOp::build(mlir::OpBuilder &builder, mlir::OperationState &state,
-                    mlir::TypeRange resultTypes, mlir::ValueRange inputs) {
-  state.addOperands(inputs);
-  auto *region = state.addRegion();
-  auto &body = region->emplaceBlock();
-  for (auto input : inputs) {
-    body.addArgument(input.getType(), input.getLoc());
-  }
-
-  state.addTypes(resultTypes);
+void TensorReadColumnOp::build(mlir::OpBuilder &builder,
+                               mlir::OperationState &state, mlir::Type result,
+                               mlir::Value scanner) {
+  build(builder, state, result, builder.getI1Type(), scanner);
 }
 
 } // namespace columnar
