9e60b7187aa04aab9ad17cb2cdc20eab68f4a813
Daan de Graaf <daandegraaf9@gmail.com>
2025-02-05 21:51:50 +0100

    TODO: linalg lowering.

--- experiments/columnar/lib/LowerToLLVM.cpp
+++ experiments/columnar/lib/LowerToLLVM.cpp
@@ -1,6 +1,14 @@
 #include "columnar/Columnar.h"
 
+#include "mlir/Conversion/ArithToLLVM/ArithToLLVM.h"
+#include "mlir/Conversion/FuncToLLVM/ConvertFuncToLLVM.h"
+#include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
+#include "mlir/Conversion/LLVMCommon/Pattern.h"
+#include "mlir/Conversion/LLVMCommon/TypeConverter.h"
+#include "mlir/Conversion/MemRefToLLVM/MemRefToLLVM.h"
+#include "mlir/Dialect/LLVMIR/FunctionCallUtils.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "llvm/Support/FormatVariadic.h"
 
 namespace columnar {
 
@@ -16,7 +24,85 @@ public:
   void runOnOperation() final;
 };
 
+class AllocStructOpLowering
+    : public mlir::ConvertOpToLLVMPattern<AllocStructOp> {
+  using mlir::ConvertOpToLLVMPattern<AllocStructOp>::ConvertOpToLLVMPattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(AllocStructOp op, OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override;
+};
+
+class GetStructElementOpLowering
+    : public mlir::ConvertOpToLLVMPattern<GetStructElementOp> {
+  using mlir::ConvertOpToLLVMPattern<
+      GetStructElementOp>::ConvertOpToLLVMPattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(GetStructElementOp op, OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override;
+};
+
 } // namespace
+
+mlir::LogicalResult AllocStructOpLowering::matchAndRewrite(
+    AllocStructOp op, OpAdaptor adaptor,
+    mlir::ConversionPatternRewriter &rewriter) const {
+  // get or insert malloc
+  auto module = op->getParentOfType<mlir::ModuleOp>();
+  auto mallocOp = mlir::LLVM::lookupOrCreateMallocFn(module, getIndexType());
+
+  auto structType = op.getResult().getType().getPointee();
+  auto llvmStructType = getTypeConverter()->convertType(structType);
+  if (!llvmStructType) {
+    return op->emitError("cannot convert struct type ")
+           << structType << " to LLVM";
+  }
+
+  // get size of the struct to allocate
+  auto size = getSizeInBytes(op.getLoc(), llvmStructType, rewriter);
+  if (!size) {
+    return op->emitError("cannot determine size of type ") << llvmStructType;
+  }
+
+  // Call to malloc
+  auto callOp = rewriter.create<mlir::LLVM::CallOp>(
+      op.getLoc(), mallocOp.getFunctionType(), mallocOp.getSymNameAttr(), size);
+
+  // Create the struct locally first
+  mlir::Value structVal =
+      rewriter.create<mlir::LLVM::UndefOp>(op.getLoc(), llvmStructType);
+  for (auto [idx, value] : llvm::enumerate(adaptor.getValues())) {
+    structVal = rewriter.create<mlir::LLVM::InsertValueOp>(
+        op.getLoc(), structVal, value, idx);
+  }
+
+  // Then write it to the heap-allocated memory
+  rewriter.create<mlir::LLVM::StoreOp>(op.getLoc(), structVal,
+                                       callOp->getResult(0));
+
+  rewriter.replaceOp(op, callOp);
+  return mlir::success();
+}
+
+mlir::LogicalResult GetStructElementOpLowering::matchAndRewrite(
+    GetStructElementOp op, OpAdaptor adaptor,
+    mlir::ConversionPatternRewriter &rewriter) const {
+  auto structType = op.getValue().getType().getPointee();
+  auto llvmStructType = getTypeConverter()->convertType(structType);
+  if (!llvmStructType) {
+    return op->emitError("cannot convert struct type ")
+           << structType << " to LLVM";
+  }
+
+  // Load the struct
+  auto loadOp = rewriter.create<mlir::LLVM::LoadOp>(op.getLoc(), llvmStructType,
+                                                    adaptor.getValue());
+  // Extract the value
+  rewriter.replaceOpWithNewOp<mlir::LLVM::ExtractValueOp>(op, loadOp,
+                                                          op.getIndex());
+  return mlir::success();
+}
 
 static mlir::LogicalResult findRuntimeCallsIn(
     mlir::Operation *op,
@@ -37,6 +123,51 @@ static mlir::LogicalResult findRuntimeCallsIn(
   });
 
   return mlir::failure(hadError);
+}
+
+static mlir::SymbolRefAttr moveToFunction(mlir::Region &region,
+                                          std::size_t pipeIdx,
+                                          llvm::StringLiteral partName,
+                                          mlir::IRRewriter &rewriter) {
+  if (region.empty()) {
+    return mlir::SymbolRefAttr();
+  }
+
+  auto name = llvm::formatv("pipe{}_{}", pipeIdx, partName).sstr<32>();
+  auto &block = region.front();
+
+  // Change terminator to ReturnOp appropriate for FuncOp.
+  mlir::func::ReturnOp returnOp;
+  {
+    mlir::OpBuilder::InsertionGuard guard(rewriter);
+    auto yieldOp = llvm::cast<PipelineLowYieldOp>(block.getTerminator());
+    rewriter.setInsertionPoint(yieldOp);
+    returnOp = rewriter.replaceOpWithNewOp<mlir::func::ReturnOp>(
+        yieldOp, yieldOp.getValues());
+  }
+
+  // Inputs types match block arguments, and the function returns whatever the
+  // block yields.
+  auto funcType = rewriter.getFunctionType(block.getArgumentTypes(),
+                                           returnOp->getOperandTypes());
+  auto funcOp =
+      rewriter.create<mlir::func::FuncOp>(region.getLoc(), name, funcType);
+  rewriter.inlineRegionBefore(region, funcOp.getBody(),
+                              funcOp.getBody().begin());
+
+  return mlir::FlatSymbolRefAttr::get(funcOp.getSymNameAttr());
+}
+
+static void pipelineMakeRef(PipelineLowOp op, std::size_t idx,
+                            mlir::IRRewriter &rewriter) {
+  auto globalOpenSym =
+      moveToFunction(op.getGlobalOpen(), idx, "globalOpen", rewriter);
+  auto bodySym = moveToFunction(op.getBody(), idx, "body", rewriter);
+  auto globalCloseSym =
+      moveToFunction(op.getGlobalClose(), idx, "globalClose", rewriter);
+
+  rewriter.replaceOpWithNewOp<PipelineRefOp>(op, globalOpenSym, bodySym,
+                                             globalCloseSym);
 }
 
 void LowerToLLVM::runOnOperation() {
@@ -66,9 +197,55 @@ void LowerToLLVM::runOnOperation() {
         op, op.getFuncAttr(), op.getResultTypes(), op.getInputs());
   }
 
-  // TODO: Replace pipeline blocks with references to functions.
+  // Replace pipeline blocks with references to functions.
+  llvm::SmallVector<PipelineLowOp> pipelines(
+      getOperation().getOps<PipelineLowOp>());
+  for (auto [i, op] : llvm::enumerate(pipelines)) {
+    rewriter.setInsertionPoint(op);
+    pipelineMakeRef(op, i, rewriter);
+  }
 
   // TODO: Invoke LLVM lowering to lower the functions.
+  mlir::LLVMConversionTarget target(getContext());
+  target.addLegalOp<mlir::ModuleOp>();
+  target.addLegalOp<PipelineRefOp>();
+
+  mlir::LLVMTypeConverter typeConverter(&getContext());
+  typeConverter.addConversion([](PointerType t) {
+    return mlir::LLVM::LLVMPointerType::get(t.getContext());
+  });
+  typeConverter.addConversion([](ScannerHandleType t) {
+    return mlir::LLVM::LLVMPointerType::get(t.getContext());
+  });
+  typeConverter.addConversion([](ColumnHandleType t) {
+    return mlir::LLVM::LLVMPointerType::get(t.getContext());
+  });
+  typeConverter.addConversion([](PrintChunkType t) {
+    return mlir::LLVM::LLVMPointerType::get(t.getContext());
+  });
+  typeConverter.addConversion([](PrintHandleType t) {
+    return mlir::LLVM::LLVMPointerType::get(t.getContext());
+  });
+  typeConverter.addConversion([&](StructType t) -> mlir::Type {
+    llvm::SmallVector<mlir::Type> types;
+    if (mlir::failed(typeConverter.convertTypes(t.getFieldTypes(), types))) {
+      return nullptr;
+    }
+
+    return mlir::LLVM::LLVMStructType::getLiteral(t.getContext(), types);
+  });
+
+  mlir::RewritePatternSet patterns(&getContext());
+  mlir::populateFuncToLLVMConversionPatterns(typeConverter, patterns);
+  mlir::arith::populateArithToLLVMConversionPatterns(typeConverter, patterns);
+  mlir::populateFinalizeMemRefToLLVMConversionPatterns(typeConverter, patterns);
+  patterns.add<AllocStructOpLowering>(typeConverter);
+  patterns.add<GetStructElementOpLowering>(typeConverter);
+
+  if (failed(
+          applyFullConversion(getOperation(), target, std::move(patterns)))) {
+    return signalPassFailure();
+  }
 }
 
 } // namespace columnar
