230ed6616dd27cbc3c240da99fbffca41de03546
Daan de Graaf <daandegraaf9@gmail.com>
2025-02-10 22:28:09 +0100

    Hooked up the runtime.

---
+++ experiments/columnar/include/columnar/Runtime.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "llvm/ExecutionEngine/Orc/CoreContainers.h"
+#include "llvm/ExecutionEngine/Orc/Mangling.h"
+
+namespace columnar {
+
+llvm::orc::SymbolMap registerRuntimeSymbols(llvm::orc::MangleAndInterner mai);
+
+} // namespace columnar
