f5e54c6c449d8ea63f62e04508d0e68a04945a32
Daan de Graaf <daandegraaf9@gmail.com>
2025-02-10 22:59:53 +0100

    Understand the memref descriptor.

--- experiments/columnar/lib/Runtime.cpp
+++ experiments/columnar/lib/Runtime.cpp
@@ -14,6 +14,15 @@ class PrintChunk;
 
 extern "C" {
 
+/**
+ * Note: I suspect the format for memref is:
+ * - void* allocated pointer
+ * - void* aligned pointer
+ * - std::size_t offset
+ * - std::size_t size
+ * - std::size_t stride
+ */
+
 TableScanner *col_table_scanner_open(TableId id) {
   llvm::errs() << "col_table_scanner_open\n";
   // TODO
@@ -41,6 +50,11 @@ void col_table_column_read(TableColumn *column, std::size_t start,
                            void *ref0, void *ref1, std::size_t ref2,
                            std::size_t ref3, std::size_t ref4) {
   llvm::errs() << "col_table_column_read\n";
+  llvm::errs() << "allocated=" << ref0 << "\n";
+  llvm::errs() << "aligned=" << ref1 << "\n";
+  llvm::errs() << "offset=" << ref2 << "\n";
+  llvm::errs() << "size=" << ref3 << "\n";
+  llvm::errs() << "stride=" << ref4 << "\n";
   // TODO
 }
 
@@ -61,12 +75,16 @@ PrintChunk *col_print_chunk_alloc(std::size_t size) {
 
 void col_print_chunk_append(PrintChunk *chunk,
                             // TODO: decode this memref
-                            void *, void *, std::size_t, std::size_t,
-                            std::size_t,
+                            void *col_alloc, void *col_align,
+                            std::size_t col_offset, std::size_t col_size,
+                            std::size_t col_stride,
                             // TODO: decode this memref
-                            void *, void *, std::size_t, std::size_t,
-                            std::size_t) {
+                            void *sel_alloc, void *sel_align,
+                            std::size_t sel_offset, std::size_t sel_size,
+                            std::size_t sel_stride) {
   llvm::errs() << "col_print_chunk_append\n";
+  llvm::errs() << "sel = " << sel_align << " offset=" << sel_offset
+               << " size=" << sel_size << " stride=" << sel_stride << "\n";
   // TODO
 }
 }
