f7226c1f2b8d6a011aaf14030f401a3ea48b944a
Daan de Graaf <daandegraaf9@gmail.com>
2026-02-16 19:12:56 +0000

    Tracking the mouse.

--- experiments/webdriver/borderify/borderify.js
+++ experiments/webdriver/borderify/borderify.js
@@ -7,3 +7,24 @@ browser.runtime.onMessage.addListener((msg) => {
 addEventListener('load', async () => {
     browser.runtime.sendMessage({});
 });
+
+addEventListener('mousemove', (event) => {
+    const target = document.querySelector('#trying_it_out');
+    if (target) {
+        const targetRect = target.firstElementChild.getBoundingClientRect();
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+
+        if (mouseX < targetRect.left) {
+            console.log("need to move right");
+        } else if (mouseX > targetRect.right) {
+            console.log("need to move left");
+        }
+
+        if (mouseY < targetRect.top) {
+            console.log("need to move down");
+        } else if (mouseY > targetRect.bottom) {
+            console.log("need to move up");
+        }
+    }
+});
