8ed048a185241f1504f25655d17a416ccefcdc09
Daan de Graaf <daandegraaf9@gmail.com>
2026-04-20 17:19:51 +0000

    Ninja to JSON.

--- experiments/dabu/ninja/dabu-ninja.cpp
+++ experiments/dabu/ninja/dabu-ninja.cpp
@@ -2,51 +2,16 @@
 #include "thirdparty/ninja-1.13.2/src/graph.h"
 #include "thirdparty/ninja-1.13.2/src/manifest_parser.h"
 #include "thirdparty/ninja-1.13.2/src/state.h"
+#include <boost/json/array.hpp>
+#include <boost/json/impl/serialize.ipp>
+#include <boost/json/object.hpp>
 #include <iostream>
 #include <unistd.h>
 
-void walk(const Node *, int);
-void walk(const Edge *, int);
-
-static void indent(int level) {
-  for (int i = 0; i < level; i++)
-    std::cout << "  ";
-}
-
-void walk(const Node *node, int level) {
-  indent(level);
-  std::cout << "node " << node->path() << "\n";
-  if (node->in_edge()) {
-    walk(node->in_edge(), level + 1);
-  }
-}
-
-void walk(const Edge *edge, int level) {
-  indent(level);
-  std::cout << "edge rule " << edge->rule().name() << "\n";
-
-  indent(level);
-  std::cout << "edge command: \n";
-  indent(level + 1);
-  std::cout << edge->EvaluateCommand() << "\n";
-
-  indent(level);
-  std::cout << "inputs:\n";
-  for (const auto *in : edge->inputs_) {
-    walk(in, level + 1);
-  }
-
-  indent(level);
-  std::cout << "outputs:\n";
-  for (const auto *out : edge->outputs_) {
-    indent(level + 1);
-    std::cout << "node (out) " << out->path() << "\n";
-  }
-}
+#include <boost/json/src.hpp>
+using namespace boost::json;
 
 int main(int argc, char **argv) {
-  std::cout << "Hello, world!\n";
-
   State state;
   RealDiskInterface diskInterface;
   ManifestParserOptions options;
@@ -58,38 +23,33 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  /*
-  auto ninjaBinary = state.LookupNode("hello");
-  if (!ninjaBinary) {
-    std::cerr << "no 'ninja' node found\n";
-    return 1;
-  }
-
-  walk(ninjaBinary, 0);
-  */
-
-  // NOTE: relevant data of edge:
-  // - command
-  // - inputs
-  // - outputs
   for (auto *edge : state.edges_) {
     const auto &rule = edge->rule();
+    object jsonEdge;
     if (rule.IsPhony()) {
-      std::cout << "phony (edge)\n";
+      jsonEdge["phony"] = true;
     } else {
       // NOTE: command is expected to be executed with 'sh -c'
+      jsonEdge["phony"] = false;
       auto cmd = edge->EvaluateCommand();
-      std::cout << "edge: " << cmd << "\n";
+      jsonEdge["command"] = cmd;
     }
 
     // NOTE: relevant data of node: path
+    array inputs;
     for (const auto *node : edge->inputs_) {
-      std::cout << "  in " << node->path() << "\n";
+      inputs.emplace_back(node->path());
     }
 
+    array outputs;
     for (const auto *node : edge->outputs_) {
-      std::cout << "  out " << node->path() << "\n";
+      outputs.emplace_back(node->path());
     }
+
+    jsonEdge["inputs"] = inputs;
+    jsonEdge["outputs"] = outputs;
+
+    std::cout << serialize(jsonEdge) << "\n";
   }
 
   return 0;
