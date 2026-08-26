09e1298f3c849a9f99aacd753541978d8f9f3234
Daan de Graaf <daandegraaf9@gmail.com>
2026-08-26 21:11:24 +0200

    Stubbing out Type.

---
+++ experiments/dblang/ir/Type.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace dblang::ir {
+
+// types:
+// - primitive (bool, char, iX, uX, f32, f64, isize, usize)
+// - def (to user-defined struct, enum or alias. can be 'unresolved')
+// - array (fixed size)
+// - pointer
+// - function (for function refs)
+
+} // namespace dblang::ir
